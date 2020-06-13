45716d3c45dbbc75d22fa157b34cb44ee53082ad
Kenneth Ibarra <>
2020-06-12 19:01:08 -0600

    Place scheme's tables.

---
+++ Scripts/createTablesPlace.sql
@@ -0,0 +1,33 @@
+-- Table Community
+CREATE TABLE COMMUNITY(
+    id_community NUMBER(6) CONSTRAINT community_id_nn NOT NULL,
+    community_name VARCHAR2(30) CONSTRAINT community_name_nn NOT NULL,
+    id_district NUMBER(6) CONSTRAINT community_iddistrict_nn NOT NULL
+    );
+    
+-- Table District
+CREATE TABLE DISTRICT(
+    id_district NUMBER(6) CONSTRAINT district_id_nn NOT NULL,
+    district_name VARCHAR2(30) CONSTRAINT district_name_nn NOT NULL,
+    id_canton NUMBER(6) CONSTRAINT district_idcanton_nn NOT NULL
+    );
+    
+-- Table Canton
+CREATE TABLE CANTON(
+    id_canton NUMBER(6) CONSTRAINT canton_id_nn NOT NULL,
+    canton_name VARCHAR2(30) CONSTRAINT canton_name_nn NOT NULL,
+    id_province NUMBER(6) CONSTRAINT canton_idprovince_nn NOT NULL
+    );
+    
+-- Table Province
+CREATE TABLE PROVINCE(
+    id_province NUMBER(6) CONSTRAINT province_id_nn NOT NULL,
+    province_name VARCHAR2(30) CONSTRAINT province_name_nn NOT NULL,
+    id_country NUMBER(6) CONSTRAINT province_idcountry_nn NOT NULL
+    );
+    
+-- Table Country
+CREATE TABLE COUNTRY(
+    id_country NUMBER(6) CONSTRAINT country_id_nn NOT NULL,
+    country_name VARCHAR2(30) CONSTRAINT country_name_nn NOT NULL
+    );
