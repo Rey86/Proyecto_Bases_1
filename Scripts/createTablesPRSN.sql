f7b2ab2c4efc976cf842e61d3d94181b028a5fb2
Kenneth Ibarra <>
2020-06-13 14:44:17 -0600

    Fix name already used by an existing constraint bug in PRSN's tables.

--- Scripts/createTablesPRSN.sql
+++ Scripts/createTablesPRSN.sql
@@ -27,7 +27,7 @@ CREATE TABLE COMPANY(
 --Table Gender
 CREATE TABLE GENDER(
     id_gender NUMBER CONSTRAINT gender_id_nn not null,
-    gender_name VARCHAR2(15) CONSTRAINT company_name_nn NOT NULL,
+    gender_name VARCHAR2(15) CONSTRAINT gender_name_nn NOT NULL,
     date_creation DATE CONSTRAINT gender_datecreation_nn NOT NULL,
     user_creation varchar2(15) CONSTRAINT gender_usercreation_nn NOT NULL,
     date_last_modification DATE CONSTRAINT gender_datelastmod_nn NOT NULL,
