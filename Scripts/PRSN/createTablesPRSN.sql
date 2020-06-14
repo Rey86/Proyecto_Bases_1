5aa259b7038febff0467f6904d9d68aa2ccb72b6
Reyner Arias <>
2020-06-14 10:55:22 -0600

    Reorganization scripts

---
+++ Scripts/PRSN/createTablesPRSN.sql
@@ -0,0 +1,35 @@
+-- Table Person
+CREATE TABLE PERSON(
+    id_user VARCHAR2(20) CONSTRAINT userapp_id_nn NOT NULL,
+    first_name VARCHAR2(25) CONSTRAINT person_firstname_nn NOT NULL,
+    last_name VARCHAR2(25) CONSTRAINT person_lastname_nn NOT NULL,
+    second_last_name VARCHAR2(25) CONSTRAINT person_secondlastname_nn NOT NULL,
+    birthdate DATE CONSTRAINT person_birthdate_nn NOT NULL, 
+    id_person VARCHAR2(20) CONSTRAINT person_id_nn NOT NULL,
+    id_gender NUMBER(6) CONSTRAINT person_idgender_nn NOT NULL,
+    id_company NUMBER(6) CONSTRAINT person_idcompany_nn NOT NULL,
+    date_creation DATE CONSTRAINT person_datecreation_nn NOT NULL,
+    user_creation varchar2(15) CONSTRAINT person_usercreation_nn NOT NULL,
+    date_last_modification DATE,
+    user_last_modification varchar2(15) CONSTRAINT
+    );
+
+-- Table Company
+CREATE TABLE COMPANY(
+    id_company NUMBER(6) CONSTRAINT company_id_nn NOT NULL,
+    company_name VARCHAR2(30) CONSTRAINT company_name_nn NOT NULL,
+    date_creation DATE CONSTRAINT company_datecreation_nn NOT NULL,
+    user_creation varchar2(15) CONSTRAINT company_usercreation_nn NOT NULL,
+    date_last_modification DATE,
+    user_last_modification varchar2(15)
+    );
+    
+--Table Gender
+CREATE TABLE GENDER(
+    id_gender NUMBER CONSTRAINT gender_id_nn not null,
+    gender_name VARCHAR2(15) CONSTRAINT gender_name_nn NOT NULL,
+    date_creation DATE CONSTRAINT gender_datecreation_nn NOT NULL,
+    user_creation varchar2(15) CONSTRAINT gender_usercreation_nn NOT NULL,
+    date_last_modification DATE,
+    user_last_modification varchar2(15)
+);
---
+++ Scripts/TR/createTablesTR.sql
@@ -0,0 +1,85 @@
+-- Table Transcript
+CREATE TABLE TRANSCRIPT(
+    valid BOOLEAN DEFAULT TRUE,
+    transcript_number VARCHAR2(15) CONSTRAINT transcript_number_nn NOT NULL,
+    username VARCHAR2(30) CONSTRAINT transcript_username_nn NOT NULL, 
+    id_accused VARCHAR2(20) CONSTRAINT transcript_idaccused_nn NOT NULL,
+    id_transcripttype NUMBER(6) CONSTRAINT transcript_idtranscripttype_nn NOT NULL,
+    id_veredict NUMBER(6) CONSTRAINT transcript_idveredict_nn NOT NULL,
+    id_community NUMBER(6) CONSTRAINT transcript_idcommunity_nn NOT NULL,
+    id_sentence NUMBER(6) CONSTRAINT transcript_idsentence_nn NOT NULL,
+    id_crime NUMBER(6) CONSTRAINT transcript_idcrime_nn NOT NULL,
+    due_date DATE CONSTRAINT transcript_duedate_nn NOT NULL,
+    date_creation DATE CONSTRAINT transcript_datecreation_nn NOT NULL,
+    user_creation varchar2(15) CONSTRAINT transcript_usercreation_nn NOT NULL,
+    date_last_modification DATE,
+    user_last_modification varchar2(15)
+    );
+    
+-- Table ProceedingsType
+CREATE TABLE TRANSCRIPTTYPE(
+    id_transcripttype NUMBER(6) CONSTRAINT transcripttype_id_nn NOT NULL,
+    transcripttype_name VARCHAR2(30) CONSTRAINT transcripttype_name_nn NOT NULL,
+    date_creation DATE CONSTRAINT transcripttype_datecreation_nn NOT NULL,
+    user_creation varchar2(15) CONSTRAINT transcripttype_usercreation_nn NOT NULL,
+    date_last_modification DATE,
+    user_last_modification varchar2(15)
+    );
+    
+-- Table Accused
+CREATE TABLE ACCUSED(
+    id_accused VARCHAR2(20) CONSTRAINT accused_id_nn NOT NULL,
+    id_company NUMBER(6) CONSTRAINT accused_idcompany_nn NOT NULL,
+    first_name VARCHAR2(25) CONSTRAINT accused_firstname_nn NOT NULL,
+    last_name VARCHAR2(25) CONSTRAINT accused_lastname_nn NOT NULL,
+    second_last_name VARCHAR2(25) CONSTRAINT accused_secondlastname_nn NOT NULL,
+    birthdate DATE CONSTRAINT accused_birthdate_nn NOT NULL, 
+    id_gender NUMBER(6) CONSTRAINT accused_idgender_nn NOT NULL,
+    date_creation DATE CONSTRAINT accused_datecreation_nn NOT NULL,
+    user_creation varchar2(15) CONSTRAINT accused_usercreation_nn NOT NULL,
+    date_last_modification DATE,
+    user_last_modification varchar2(15)
+    );
+
+-- Table Veredict
+CREATE TABLE VERDICT(
+    id_veredict NUMBER(6) CONSTRAINT veredict_id_nn NOT NULL,
+    veredict_name VARCHAR2(30) CONSTRAINT veredict_name_nn NOT NULL,
+    date_creation DATE CONSTRAINT veredict_datecreation_nn NOT NULL,
+    user_creation varchar2(15) CONSTRAINT veredict_usercreation_nn NOT NULL,
+    date_last_modification DATE,
+    user_last_modification varchar2(15)
+    );
+    
+--Table Sentence
+CREATE TABLE SENTENCE(
+    id_sentence NUMBER(6) CONSTRAINT sentence_id_nn NOT NULL,
+    sentence_name VARCHAR2(30) CONSTRAINT sentence_name_nn NOT NULL,
+    start_date DATE CONSTRAINT sentence_startdate_nn NOT NULL,
+    end_date DATE CONSTRAINT sentence_enddate_nn NOT NULL,
+    id_sentencetype NUMBER(6) CONSTRAINT sentence_typeid_nn NOT NULL,
+    date_creation DATE CONSTRAINT sentence_datecreation_nn NOT NULL,
+    user_creation varchar2(15) CONSTRAINT sentence_usercreation_nn NOT NULL,
+    date_last_modification DATE,
+    user_last_modification varchar2(15)
+    );  
+    
+--Table SentenceType
+CREATE TABLE SENTENCETYPE(
+    id_sentencetype NUMBER(6) CONSTRAINT sentencetype_id_nn NOT NULL,
+    sentencetype_name VARCHAR2(30) CONSTRAINT sentencetype_name_nn NOT NULL,
+    date_creation DATE CONSTRAINT sentencetype_datecreation_nn NOT NULL,
+    user_creation varchar2(15) CONSTRAINT sentencetype_usercreation_nn NOT NULL,
+    date_last_modification DATE,
+    user_last_modification varchar2(15)
+    );
+    
+--Table Crime
+CREATE TABLE CRIME(
+    id_crime NUMBER(6) CONSTRAINT crime_id_nn NOT NULL,
+    crime_name VARCHAR2(30) CONSTRAINT crime_name_nn NOT NULL,
+    date_creation DATE CONSTRAINT crime_datecreation_nn NOT NULL,
+    user_creation varchar2(15) CONSTRAINT crime_usercreation_nn NOT NULL,
+    date_last_modification DATE,
+    user_last_modification varchar2(15)
+    );
