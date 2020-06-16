083d7ff1e98babc361d44c8066b87c34785dc6fc
Reyner Arias <>
2020-06-16 15:07:17 -0600

    Modify getCommunity procedure

--- Scripts/Place/createPackagePlace.sql
+++ Scripts/Place/createPackagePlace.sql
@@ -1,32 +1,27 @@
 -- Package to Place procedures and functions.
 CREATE OR REPLACE PACKAGE PlaceTables IS
     -- Community Table
-    FUNCTION getAllCommunities RETURN VARCHAR2; 
-    FUNCTION getCommunityColumns (pnIdCommunity NUMBER) RETURN VARCHAR2;
+    PROCEDURE getCommunity (pnIdCommunity NUMBER);
     PROCEDURE setCommunityColumns (pnIdCommunity NUMBER, pcCommunityName VARCHAR2, pnIdDistrict NUMBER);
     PROCEDURE deleteCommunity (pnIdCommunity NUMBER);
     PROCEDURE insertCommunity (pcCommunityName VARCHAR2, pnIdDistrict NUMBER);
     -- District Table
-    FUNCTION getAllDistricts RETURN VARCHAR2; 
-    FUNCTION getDistrictColumns (pnIdDistrict NUMBER) RETURN VARCHAR2;
+    PROCEDURE getDistrict (pnIdDistrict NUMBER);
     PROCEDURE setDistrictColumns (pnIdDistrict NUMBER, pcDistrictName VARCHAR2, pnIdCanton NUMBER);
     PROCEDURE deleteDistrict (pnIdDistrict NUMBER);
     PROCEDURE insertDistrict (pcDistrictName VARCHAR2, pnIdCanton NUMBER);
-    -- Canton Table
-    FUNCTION getAllCantons RETURN VARCHAR2; 
-    FUNCTION getCantonColumns (pnIdCanton NUMBER) RETURN VARCHAR2;
+    -- Canton Table 
+    PROCEDURE getCanton (pnIdCanton NUMBER);
     PROCEDURE setCantonColumns (pnIdCanton NUMBER, pcCantonName VARCHAR2, pnIdProvince NUMBER);
     PROCEDURE deleteCanton (pnIdCanton NUMBER);
     PROCEDURE insertCanton (pcCantonName VARCHAR2, pnIdProvince NUMBER);
-    -- Province Table
-    FUNCTION getAllProvinces RETURN VARCHAR2; 
-    FUNCTION getProvinceColumns (pnIdProvince NUMBER) RETURN VARCHAR2;
+    -- Province Table 
+    PROCEDURE getProvince (pnIdProvince NUMBER);
     PROCEDURE setProvinceColumns (pnIdProvince NUMBER, pcProvinceName VARCHAR2, pnIdCountry NUMBER);
     PROCEDURE deleteProvince (pnIdProvince NUMBER);
     PROCEDURE insertProvince (pcProvinceName VARCHAR2, pnIdCountry NUMBER);
     -- Country Table
-    FUNCTION getAllCountries RETURN VARCHAR2; 
-    FUNCTION getCountryColumns (pnIdCountry NUMBER) RETURN VARCHAR2;
+    PROCEDURE getCountry (pnIdCountry NUMBER);
     PROCEDURE setCountryColumns (pnIdCountry NUMBER, pcCountryName VARCHAR2);
     PROCEDURE deleteCountry (pnIdCountry NUMBER);
     PROCEDURE insertCountry (pcCountryName VARCHAR2);
@@ -34,25 +29,21 @@ END PlaceTables;
 
 CREATE OR REPLACE PACKAGE BODY PlaceTables AS
 -- Table Community
--- Function to get all communities to show them in the screen
-    FUNCTION getAllCommunities RETURN VARCHAR2 IS
-        vcCommunityName VARCHAR2(30);
+-- Function to get a community with specific id to show it in the screen      
+    PROCEDURE getCommunity (pnIdCommunity IN NUMBER) AS
+    CURSOR community(pnIdCommunity IN NUMBER)
+    IS
+        SELECT c.id_community id_community, c.community_name community_name, d.district_name district_name
+        FROM COMMUNITY c
+        INNER JOIN DISTRICT d
+        ON c.id_district = d.id_district
+        WHERE d.id_community = NVL(pnIdCommunity, d.id_community);
     BEGIN 
-        SELECT community_name
-        INTO vcCommunityName
-        FROM COMMUNITY;
-        RETURN(vcCommunityName);    
-    END getCommunityColumns;
-
--- Function to get a community with specific id to show it in the screen  
-    FUNCTION getCommunityColumns (pnIdCommunity NUMBER) RETURN VARCHAR2 IS
-        vcCommunityName VARCHAR2(30);
-    BEGIN 
-        SELECT community_name
-        INTO vcCommunityName
-        FROM COMMUNITY
-        WHERE id_community = pnIdCommunity;
-        RETURN(vcCommunityName);    
+        FOR i in community(pnIdCommunity) LOOP
+            dbms_output.put_line(i.id_community);
+            dbms_output.put_line(i.community_name);
+            dbms_output.put_line(i.district_name);
+        END LOOP;
     END getCommunityColumns;
 
 -- Procedure to set a community with specific id and the new values wrote by the user  
