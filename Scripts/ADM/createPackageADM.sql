35093565dfb27ae333f4e235dd77a065c5facd04
Reyner Arias <>
2020-06-16 14:50:55 -0600

    Change getParameter to cursor

--- Scripts/ADM/createPackageADM.sql
+++ Scripts/ADM/createPackageADM.sql
@@ -1,32 +1,25 @@
 -- Package to ADM procedures and functions.
 CREATE OR REPLACE PACKAGE ADMTables IS
-    FUNCTION getAllParameters RETURN VARCHAR2; 
-    FUNCTION getParameterColumns (pnIdParameter NUMBER) RETURN VARCHAR2;
+    PROCEDURE getParameter (pnIdParameter NUMBER);
     PROCEDURE setParameterColumns (pnIdParameter NUMBER, pcParameterName VARCHAR2, pnParameterValue NUMBER);
     PROCEDURE deleteParameter (pnIdParameter NUMBER);
     PROCEDURE insertParameter (pcParameterName VARCHAR2, pnParameterValue NUMBER);
 END ADMTables;
 
 CREATE OR REPLACE PACKAGE BODY ADMTables AS
--- Function to get all parameters to show them in the screen
-    FUNCTION getAllParameters RETURN VARCHAR2 IS
-        vcParameterName VARCHAR2(30);
-    BEGIN 
-        SELECT parameter_name
-        INTO vcParameterName
-        FROM PARAMETER_TABLE;
-        RETURN(vcParameterName);    
-    END getParameterColumns;
-
--- Function to get a parameter with specific id to show it in the screen  
-    FUNCTION getParameterColumns (pnIdParameter NUMBER) RETURN VARCHAR2 IS
-        vcParameterName VARCHAR2(30);
-    BEGIN 
-        SELECT parameter_name
-        INTO vcParameterName
+-- Procedure to get a parameter with specific id to show it in the screen  
+    PROCEDURE getParameter (pnIdParameter IN NUMBER) AS
+    CURSOR parameterTable(pnIdParameter IN NUMBER)
+    IS
+        SELECT id_parameter, parameter_name, parameter_value
         FROM PARAMETER_TABLE
-        WHERE id_parameter = pnIdParameter;
-        RETURN(vcParameterName);    
+        WHERE id_parameter = NVL(pnIdParameter, id_parameter);
+    BEGIN 
+        FOR i in parameterTable(pnIdParameter) LOOP
+            dbms_output.put_line(i.id_parameter);
+            dbms_output.put_line(i.parameter_name);
+            dbms_output.put_line(i.parameter_value);
+        END LOOP;
     END getParameterColumns;
 
 -- Procedure to set a parameter with specific id and the new values wrote by the user  
