ffa18b62911ae97e1828f0b31adceeaa97400fea
Kenneth <Kenneth@DESKTOP-D3OL4OG>
2020-06-19 21:42:11 -0600

    Add comments to ADM's tables and columns.

--- Scripts/ADM/createTablesADM.sql
+++ Scripts/ADM/createTablesADM.sql
@@ -8,6 +8,30 @@ CREATE TABLE GENERALLOG(
     currentValue VARCHAR2(20) CONSTRAINT generallog_currentValue_nn NOT NULL,
     previousValue VARCHAR2(20)
     );
+--Comments
+COMMENT ON TABLE GENERALLOG
+    IS 'Table that register every change of value in the database';
+COMMENT ON COLUMN
+    GENERALLOG.id_generallog IS
+    'Log identification number';
+COMMENT ON COLUMN
+    GENERALLOG.scheme_name IS
+    'Name of scheme that underwent change';
+COMMENT ON COLUMN
+    GENERALLOG.table_name IS
+    'Name of table that underwent change';
+COMMENT ON COLUMN
+    GENERALLOG.column_name IS
+    'Name of column that underwent change';
+COMMENT ON COLUMN
+    GENERALLOG.modification_date IS
+    'Date of the modification';
+COMMENT ON COLUMN
+    GENERALLOG.currentValue IS
+    'Actual value';
+COMMENT ON COLUMN
+    GENERALLOG.previousValue IS
+    'Old value';
     
 --Table Parameter
 CREATE TABLE PARAMETER_TABLE(
@@ -19,3 +43,27 @@ CREATE TABLE PARAMETER_TABLE(
     date_last_modification DATE,
     user_last_modification VARCHAR2(15)
     );
+--Comments
+COMMENT ON TABLE PARAMETER_TABLE
+    IS 'Table that stores parameters for querys';
+COMMENT ON COLUMN
+    PARAMETER_TABLE.id_parameter IS
+    'Parameter identification number';
+COMMENT ON COLUMN
+    PARAMETER_TABLE.parameter_name IS
+    'Parameter name';
+COMMENT ON COLUMN
+    PARAMETER_TABLE.parameter_value IS
+    'Parameter value';
+COMMENT ON COLUMN
+    PARAMETER_TABLE.date_creation IS
+    'Date of creation';
+COMMENT ON COLUMN
+    PARAMETER_TABLE.user_creation IS
+    'User who created it';
+COMMENT ON COLUMN
+    PARAMETER_TABLE.date_last_modification IS
+    'Date of the last modification';
+COMMENT ON COLUMN
+    PARAMETER_TABLE.user_last_modification IS
+    'Last user who modified it';
