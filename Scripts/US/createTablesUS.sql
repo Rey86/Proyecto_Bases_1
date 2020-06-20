043ca1772d3c1ffb4e65d8bd00df56a69cfdbdf1
Kenneth <Kenneth@DESKTOP-D3OL4OG>
2020-06-20 11:32:10 -0600

    Add comments to US's tables and columns.

--- Scripts/US/createTablesUS.sql
+++ Scripts/US/createTablesUS.sql
@@ -19,6 +19,57 @@ CREATE TABLE USERAPP(
     date_last_modification DATE,
     user_last_modification VARCHAR2(15)
     );
+--Comments
+COMMENT ON TABLE USERAPP
+    IS 'Table that stores data of users';
+COMMENT ON COLUMN
+    USERAPP.username IS
+    'Identification name of the user';
+COMMENT ON COLUMN
+    USERAPP.user_password IS
+    'Password of the user';
+COMMENT ON COLUMN
+    USERAPP.ban_days IS
+    'Number of days that the user will be bannned';
+COMMENT ON COLUMN
+    USERAPP.banned IS
+    'Number that shows if the user is banned, 0 = false and 1 = true';
+COMMENT ON COLUMN
+    USERAPP.id_usertype IS
+    'Identification number of user type of the user';
+COMMENT ON COLUMN
+    USERAPP.first_name IS
+    'First name of the user';
+COMMENT ON COLUMN
+    USERAPP.last_name IS
+    'Last name of the user';
+COMMENT ON COLUMN
+    USERAPP.second_last_name IS
+    'Second last name of the user';
+COMMENT ON COLUMN
+    USERAPP.birthdate IS
+    'Date of birth of the user';
+COMMENT ON COLUMN
+    USERAPP.id_user IS
+    'Identification number of the user';
+COMMENT ON COLUMN
+    USERAPP.id_gender IS
+    'Identification number of gender of the user';
+COMMENT ON COLUMN
+    USERAPP.id_company IS
+    'Identification number of company of the user';
+COMMENT ON COLUMN
+    USERAPP.date_creation IS
+    'Date of creation';
+COMMENT ON COLUMN
+    USERAPP.user_creation IS
+    'User who created it';
+COMMENT ON COLUMN
+    USERAPP.date_last_modification IS
+    'Date of the last modification';
+COMMENT ON COLUMN
+    USERAPP.user_last_modification IS
+    'Last user who modified it';
     
 -- Table UserType
 CREATE TABLE USERTYPE(
@@ -29,6 +80,27 @@ CREATE TABLE USERTYPE(
     date_last_modification DATE,
     user_last_modification VARCHAR2(15)
     );
+--Comments
+COMMENT ON TABLE USERTYPE
+    IS 'Table that stores data of user types';
+COMMENT ON COLUMN
+    USERTYPE.id_usertype IS
+    'Identification number of the user type';
+COMMENT ON COLUMN
+    USERTYPE.usertype_description IS
+    'Description of the user type';
+COMMENT ON COLUMN
+    USERTYPE.date_creation IS
+    'Date of creation';
+COMMENT ON COLUMN
+    USERTYPE.user_creation IS
+    'User who created it';
+COMMENT ON COLUMN
+    USERTYPE.date_last_modification IS
+    'Date of the last modification';
+COMMENT ON COLUMN
+    USERTYPE.user_last_modification IS
+    'Last user who modified it';
     
 -- Table BanReason
 CREATE TABLE BANREASON(
@@ -39,9 +111,55 @@ CREATE TABLE BANREASON(
     date_last_modification DATE,
     user_last_modification VARCHAR2(15)
     );
+--Comments
+COMMENT ON TABLE BANREASON
+    IS 'Table that stores data of ban reasons';
+COMMENT ON COLUMN
+    BANREASON.id_banreason IS
+    'Identification number of the ban reason';
+COMMENT ON COLUMN
+    BANREASON.banreason_description IS
+    'Description of the ban reason';
+COMMENT ON COLUMN
+    BANREASON.date_creation IS
+    'Date of creation';
+COMMENT ON COLUMN
+    BANREASON.user_creation IS
+    'User who created it';
+COMMENT ON COLUMN
+    BANREASON.date_last_modification IS
+    'Date of the last modification';
+COMMENT ON COLUMN
+    BANREASON.user_last_modification IS
+    'Last user who modified it';
     
 --Table BanReasonXUserapp
 CREATE TABLE BANREASONXUSERAPP(
-    id_banreason NUMBER(8) CONSTRAINT banreason_id_nn NOT NULL,
-    username VARCHAR2(30) CONSTRAINT userapp_username_nn NOT NULL
+    id_banreason NUMBER(8) CONSTRAINT brXu_idBan_nn NOT NULL,
+    username VARCHAR2(30) CONSTRAINT brXu_username_nn NOT NULL,
+    date_creation DATE CONSTRAINT brXu_datecreation_nn NOT NULL,
+    user_creation VARCHAR2(15) CONSTRAINT brXu_usercreation_nn NOT NULL,
+    date_last_modification DATE,
+    user_last_modification VARCHAR2(15)
 );
+--Comments
+COMMENT ON TABLE BANREASONXUSERAPP
+    IS 'Table that stores data of ban reason for each username';
+COMMENT ON COLUMN
+    BANREASONXUSERAPP.id_banreason IS
+    'Identification number of the ban reason';
+COMMENT ON COLUMN
+    BANREASONXUSERAPP.username IS
+    'Identification name of the user';
+COMMENT ON COLUMN
+    BANREASONXUSERAPP.date_creation IS
+    'Date of creation';
+COMMENT ON COLUMN
+    BANREASONXUSERAPP.user_creation IS
+    'User who created it';
+COMMENT ON COLUMN
+    BANREASONXUSERAPP.date_last_modification IS
+    'Date of the last modification';
+COMMENT ON COLUMN
+    BANREASONXUSERAPP.user_last_modification IS
+    'Last user who modified it';
