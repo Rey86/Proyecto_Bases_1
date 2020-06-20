4a53f40c84a9a187b6b333897a8ee02ef1d82df9
Kenneth <Kenneth@DESKTOP-D3OL4OG>
2020-06-19 22:04:16 -0600

    Add comments to PRSN's tables and columns.

--- Scripts/PRSN/createTablesPRSN.sql
+++ Scripts/PRSN/createTablesPRSN.sql
@@ -12,6 +12,42 @@ CREATE TABLE PERSON(
     date_last_modification DATE,
     user_last_modification VARCHAR2(15)
     );
+--Comments
+COMMENT ON TABLE PERSON
+    IS 'Table that stores data of people';
+COMMENT ON COLUMN
+    PERSON.first_name IS
+    'First name of the person';
+COMMENT ON COLUMN
+    PERSON.last_name IS
+    'Last name of the person';
+COMMENT ON COLUMN
+    PERSON.second_last_name IS
+    'Second last name of the person';
+COMMENT ON COLUMN
+    PERSON.birthdate IS
+    'Date of birth of the person';
+COMMENT ON COLUMN
+    PERSON.id_person IS
+    'Identification number of the person';
+COMMENT ON COLUMN
+    PERSON.id_gender IS
+    'Identification number of gender of the person';
+COMMENT ON COLUMN
+    PERSON.id_company IS
+    'Identification number of company of the person';
+COMMENT ON COLUMN
+    PERSON.date_creation IS
+    'Date of creation';
+COMMENT ON COLUMN
+    PERSON.user_creation IS
+    'User who created it';
+COMMENT ON COLUMN
+    PERSON.date_last_modification IS
+    'Date of the last modification';
+COMMENT ON COLUMN
+    PERSON.user_last_modification IS
+    'Last user who modified it';
 
 -- Table Company
 CREATE TABLE COMPANY(
@@ -22,6 +58,27 @@ CREATE TABLE COMPANY(
     date_last_modification DATE,
     user_last_modification VARCHAR2(15)
     );
+--Comments
+COMMENT ON TABLE COMPANY
+    IS 'Table that stores data of companies';
+COMMENT ON COLUMN
+    COMPANY.id_company IS
+    'Identification number of the company';
+COMMENT ON COLUMN
+    COMPANY.company_name IS
+    'Name of the company';
+COMMENT ON COLUMN
+    COMPANY.date_creation IS
+    'Date of creation';
+COMMENT ON COLUMN
+    COMPANY.user_creation IS
+    'User who created it';
+COMMENT ON COLUMN
+    COMPANY.date_last_modification IS
+    'Date of the last modification';
+COMMENT ON COLUMN
+    COMPANY.user_last_modification IS
+    'Last user who modified it';
     
 --Table Gender
 CREATE TABLE GENDER(
@@ -32,3 +89,24 @@ CREATE TABLE GENDER(
     date_last_modification DATE,
     user_last_modification VARCHAR2(15)
 );
+--Comments
+COMMENT ON TABLE GENDER
+    IS 'Table that stores data of genders';
+COMMENT ON COLUMN
+    GENDER.id_gender IS
+    'Identification number of the gender';
+COMMENT ON COLUMN
+    GENDER.gender_name IS
+    'Name of the gender';
+COMMENT ON COLUMN
+    GENDER.date_creation IS
+    'Date of creation';
+COMMENT ON COLUMN
+    GENDER.user_creation IS
+    'User who created it';
+COMMENT ON COLUMN
+    GENDER.date_last_modification IS
+    'Date of the last modification';
+COMMENT ON COLUMN
+    GENDER.user_last_modification IS
+    'Last user who modified it';
