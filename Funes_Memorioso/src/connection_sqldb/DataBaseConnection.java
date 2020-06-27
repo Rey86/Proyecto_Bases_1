7c224ef55afb178593d31f8b1a39232e94d28aab
Rey86 <Rey86582@gmail.com>
2020-06-27 10:34:25 -0600

    Add procedures and functions to control ADM.

--- Funes_Memorioso/src/connection_sqldb/DataBaseConnection.java
+++ Funes_Memorioso/src/connection_sqldb/DataBaseConnection.java
@@ -5,19 +5,129 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.sql.Types;
 import oracle.jdbc.OracleTypes;
 
 public class DataBaseConnection {
+    // Function that returns a connection with the data base
     public static Connection getConnectionDataBase() throws SQLException{
-            String host = "jdbc:oracle:thin:@localhost:1521:DBPROJECT1";
-            String uName = "HR";
-            String uPass = "HR";
-        
-            Connection con = DriverManager.getConnection(host, uName, uPass);
-            return con;
+        String host = "jdbc:oracle:thin:@localhost:1521:DBPROJECT1";
+        String uName = "HR";
+        String uPass = "HR";
+
+        Connection con = DriverManager.getConnection(host, uName, uPass);
+        return con;
     }
     
-    public static void insertCommunity(String pcCommunityName, int pnIdDistrict) throws SQLException{
+    // Function to get all parameters of the system
+    public static ArrayList<String> getParameters() throws SQLException{
+        ArrayList<String> parameters = new ArrayList<String>();
+        Connection con = getConnectionDataBase();
+        CallableStatement stmt = con.prepareCall("{?= call ADM.ADMTables.getParameter(?)}");
+        stmt.registerOutParameter(1, OracleTypes.CURSOR);
+        stmt.setNull(2, Types.INTEGER);
+        stmt.executeQuery();
+        ResultSet r = (ResultSet) stmt.getObject(1);
+        
+        while(r.next()){
+            parameters.add("ID: " + Integer.toString(r.getInt("ID_PARAMETER")) + " Nombre: " + 
+                r.getString("PARAMETER_NAME") + " Valor: " + Integer.toString(r.getInt("PARAMETER_VALUE")));
+        }
+        return parameters;
+    }
+    
+    // Function to get a parameter of the system
+    public static ResultSet getParameter(Integer pnIdParameter) throws SQLException{
+        Connection con = getConnectionDataBase();
+        CallableStatement stmt = con.prepareCall("{?= call ADM.ADMTables.getParameter(?)}");
+        stmt.registerOutParameter(1, OracleTypes.CURSOR);
+        stmt.setInt(2, pnIdParameter);
+        stmt.executeQuery();
+        ResultSet r = (ResultSet) stmt.getObject(1);
+        return r;
+    }
+    
+    // Function to set a parameter of the system
+    public static void setParameter(Integer pnIdParameter, String pcParameterName, Integer pnParameterValue) throws SQLException{
+        Connection con = getConnectionDataBase();
+        CallableStatement stmt = con.prepareCall("{ call ADM.ADMTables.setParameter(?,?,?)}");
+        
+        stmt.setInt(1, pnIdParameter);
+        stmt.setString(2, pcParameterName);
+        stmt.setInt(3, pnParameterValue);
+        stmt.execute();
+    }
+    
+    // Function to delete a parameter of the system
+    public static void deleteParameter(Integer pnIdParameter) throws SQLException{
+        Connection con = getConnectionDataBase();
+        CallableStatement stmt = con.prepareCall("{ call ADM.ADMTables.deleteParameter(?)}");
+        
+        stmt.setInt(1, pnIdParameter);
+        stmt.execute();
+    }
+    
+    // Function to insert a parameter of the system
+    public static void insertParameter(String pcParameterName, Integer pnParameterValue) throws SQLException{
+        Connection con = getConnectionDataBase();
+        CallableStatement stmt = con.prepareCall("{ call ADM.ADMTables.insertParamer(?,?)}");
+        
+        stmt.setString(1, pcParameterName);
+        stmt.setInt(2, pnParameterValue);
+        stmt.execute();
+    }
+    
+    // Function to get all parameters of the system
+    public static ArrayList<String> getCommunities() throws SQLException{
+        ArrayList<String> parameters = new ArrayList<String>();
+        Connection con = getConnectionDataBase();
+        CallableStatement stmt = con.prepareCall("{?= call ADM.ADMTables.getParameter(?)}");
+        stmt.registerOutParameter(1, OracleTypes.CURSOR);
+        stmt.setNull(2, Types.INTEGER);
+        stmt.executeQuery();
+        ResultSet r = (ResultSet) stmt.getObject(1);
+        
+        while(r.next()){
+            parameters.add("ID: " + Integer.toString(r.getInt("ID_PARAMETER")) + " Nombre: " + 
+                r.getString("PARAMETER_NAME") + " Valor: " + Integer.toString(r.getInt("PARAMETER_VALUE")));
+        }
+        return parameters;
+    }
+    
+    // Function to get all parameters of the system
+    public static ResultSet getCommunity(Integer pnIdParameter) throws SQLException{
+        Connection con = getConnectionDataBase();
+        CallableStatement stmt = con.prepareCall("{?= call ADM.ADMTables.getParameter(?)}");
+        stmt.registerOutParameter(1, OracleTypes.CURSOR);
+        stmt.setInt(2, pnIdParameter);
+        stmt.executeQuery();
+        ResultSet r = (ResultSet) stmt.getObject(1);
+        return r;
+    }
+    
+    // Function to set a parameter of the system
+    public static void setCommunity(Integer pnIdParameter, String pcParameterName, Integer pnParameterValue) throws SQLException{
+        Connection con = getConnectionDataBase();
+        CallableStatement stmt = con.prepareCall("{ call ADM.ADMTables.setParameter(?,?,?)}");
+        
+        stmt.setInt(1, pnIdParameter);
+        stmt.setString(2, pcParameterName);
+        stmt.setInt(3, pnParameterValue);
+        stmt.execute();
+    }
+    
+    // Function to delete a parameter of the system
+    public static void deleteCommunity(Integer pnIdParameter) throws SQLException{
+        Connection con = getConnectionDataBase();
+        CallableStatement stmt = con.prepareCall("{ call ADM.ADMTables.deleteParameter(?)}");
+        
+        stmt.setInt(1, pnIdParameter);
+        stmt.execute();
+    }
+    
+    // Procedure to insert a community in the system
+    public static void insertCommunity(String pcCommunityName, Integer pnIdDistrict) throws SQLException{
         Connection con = getConnectionDataBase();
         CallableStatement stmt = con.prepareCall("{ call Place.PlaceTables.insertCommunity(?,?)}");
         
@@ -26,6 +136,7 @@ public class DataBaseConnection {
         stmt.execute();
     }
     
+    // Procedure to insert a country in the system
     public static void insertCountry(String pcCountryName) throws SQLException{
         Connection con = getConnectionDataBase();
         CallableStatement stmt = con.prepareCall("{ call Place.PlaceTables.insertCountry(?)}");
@@ -34,11 +145,12 @@ public class DataBaseConnection {
         stmt.execute();
     }
     
-    public static void getCountry(int pnIdCountry) throws SQLException{
+    // Function to get a country of the system
+    public static void getCountry() throws SQLException{
         Connection con = getConnectionDataBase();
         CallableStatement stmt = con.prepareCall("{?= call Place.PlaceTables.getCountry(?)}");
         stmt.registerOutParameter(1, OracleTypes.CURSOR);
-        stmt.setInt(2, pnIdCountry);
+        stmt.setNull(2, Types.INTEGER);
         stmt.executeQuery();
         ResultSet r = (ResultSet) stmt.getObject(1);
         
@@ -47,8 +159,17 @@ public class DataBaseConnection {
         }
     }
     
-    public static void getTopSentenceTime(int n) throws SQLException {
+    // Function to get the top sentence time in the base's transcripts
+    public static void getTopSentenceTime(Integer n) throws SQLException {
         Connection con = getConnectionDataBase();
-        CallableStatement stmt = con.prepareCall("{?= call Place.PlaceTables.getCountry(?)}");
+        CallableStatement stmt = con.prepareCall("{?= call TR.TRUserReports.topSentenceTime(?)}");
+        stmt.registerOutParameter(1, OracleTypes.CURSOR);
+        stmt.setInt(2, n);
+        stmt.executeQuery();
+        ResultSet r = (ResultSet) stmt.getObject(1);
+        
+        while(r.next()){
+            System.out.println(r.getString("SENTENCE_TIME"));
+        }
     }
 }
