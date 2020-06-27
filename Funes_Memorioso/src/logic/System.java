ed100cd97003a9a91ea26f28bdeb75deeb755881
Rey86 <Rey86582@gmail.com>
2020-06-26 18:06:31 -0600

    Add insert country procedure.

--- Funes_Memorioso/src/logic/System.java
+++ Funes_Memorioso/src/logic/System.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package logic;
 
-/**
- *
- * @author Reyner
- */
+import connection_sqldb.DataBaseConnection;
+import java.sql.SQLException;
+
 public class System {
-    /**
-     * @param args the command line arguments
-     */
-    /*public static void main(String[] args) {
-        // TODO code application logic here
-    }*/
+    public static void main(String[] args) throws SQLException {
+        DataBaseConnection.insertCountry("Costa Rica");
+    }
 }
