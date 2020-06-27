6061048be099638620db0da21b36195b589208ab
Rey86 <Rey86582@gmail.com>
2020-06-26 19:48:54 -0600

    Add data base connection.

--- Funes_Memorioso/src/connection_sqldb/DataBaseConnection.java
+++ Funes_Memorioso/src/connection_sqldb/DataBaseConnection.java
@@ -1,5 +1,36 @@
 package connection_sqldb;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import oracle.jdbc.OracleTypes;
+
 public class DataBaseConnection {
+    public static Connection getConnectionDataBase() throws SQLException{
+        String host = "jdbc:oracle:thin:@localhost:1521:DBPROJECT1";
+        String uName = "HR";
+        String uPass = "HR";
+        
+        Connection con = DriverManager.getConnection(host, uName, uPass);
+        return con;
+    }
     
+    public static void insertCommunity(String pcCommunityName, int pnIdDistrict) throws SQLException{
+        Connection con = getConnectionDataBase();
+        CallableStatement stmt = con.prepareCall("{ call Place.PlaceTables.insertCommunity(?,?)}");
+        
+        stmt.setString(1, pcCommunityName);
+        stmt.setInt(2, pnIdDistrict);
+        stmt.execute();
+    }
+    
+    public static void insertCountry(String pcCountryName) throws SQLException{
+        Connection con = getConnectionDataBase();
+        CallableStatement stmt = con.prepareCall("{ call Place.PlaceTables.insertCountry(?)}");
+        
+        stmt.setString(1, pcCountryName);
+        stmt.execute();
+    }
 }
